fd603f2887380a583e8cea6527c47125b1ac08a0
Kevin-dev-java <kivim2018@gmail.com>
2021-03-31 18:11:05 -0300

    User entity and resource

--- src/main/java/com/kevinjava/course/CourseApplication.java
+++ src/main/java/com/kevinjava/course/CourseApplication.java
@@ -1,4 +1,4 @@
-package com.kevinjava.workshop;
+package com.kevinjava.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
