c22f75ed87ffeb5d071785bc309b8d16d2ff14e8
Kevin-dev-java <kivim2018@gmail.com>
2021-03-31 16:12:04 -0300

    Project created

---
+++ src/test/java/com/kevinjava/workshop/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinjava.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
