ddbaf270061ce430ef1e2040ab0ec6b59d1a3cb9
Kevin-dev-java <kivim2018@gmail.com>
2021-04-03 14:08:54 -0300

    Category

--- src/main/java/com/kevinjava/course/config/TestConfig.java
+++ src/main/java/com/kevinjava/course/config/TestConfig.java
@@ -10,8 +10,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.kevinjava.course.entities.Category;
 import com.kevinjava.course.entities.Order;
 import com.kevinjava.course.entities.User;
+import com.kevinjava.course.repositories.CategoryRepository;
 import com.kevinjava.course.repositories.OrderRepository;
 import com.kevinjava.course.repositories.UserRepository;
 
@@ -24,9 +26,20 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
-
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888888", "12345");
 		User u2 = new User(null, "Alex Green", "Alex@gmail.com", "9777777777", "12345678");
@@ -34,6 +47,7 @@ public class TestConfig implements CommandLineRunner{
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
