8195ff82f310a19ee39ecd8feffb36275593c2cb
Kevin-dev-java <kivim2018@gmail.com>
2021-04-07 15:54:31 -0300

    Sub total & total payment

--- src/main/java/com/kevinjava/course/entities/OrderItem.java
+++ src/main/java/com/kevinjava/course/entities/OrderItem.java
@@ -63,10 +63,14 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	} 
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
+		final int prime = 31; 
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
